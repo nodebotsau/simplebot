8769b08bba32e65dc47f0be97afabadc63f0b316
= <ajfisher.td@gmail.com>
2014-10-06 20:09:46 +1100

    cleaned code, fixed formatting issues

--- examples/simplebot.js
+++ examples/simplebot.js
@@ -6,7 +6,6 @@
 var five = require("johnny-five");
 var keypress = require('keypress');
 
-var STOPVAL = 90;
 var RSTOPVAL = 93;
 var LSTOPVAL = 90;
 
@@ -19,7 +18,6 @@ var board = new five.Board(opts);
 
 board.on("ready", function() {
 
-    console.log("Welcome to SimpleBot!")
     console.log("Control the bot with the arrow keys, and SPACE to stop.")
 
     var left_wheel  = new five.Servo({ pin:  9, type: 'continuous' }).to(LSTOPVAL);
@@ -30,55 +28,44 @@ board.on("ready", function() {
     process.stdin.setRawMode(true); 
 
     process.stdin.on('keypress', function (ch, key) {
-    
-    if ( !key ) return;
 
-    if ( key.name == 'q' ) {
+        if ( !key ) return;
 
-        console.log('Quitting');
-        process.exit();
+        if ( key.name == 'q' ) {
+            console.log('Quitting');
+            process.exit();
+        } else if ( key.name == 'up' ) {
 
-    } else if ( key.name == 'up' ) {
+            console.log('Forward');
+            left_wheel.cw();
+            right_wheel.ccw();
 
-        console.log('Forward');
-        left_wheel.cw();
-        right_wheel.ccw();
+        } else if ( key.name == 'down' ) {
 
-    } else if ( key.name == 'down' ) {
+            console.log('Backward');
+            left_wheel.ccw();
+            right_wheel.cw();      
 
-        console.log('Backward');
-        left_wheel.ccw();
-        right_wheel.cw();      
+        } else if ( key.name == 'left' ) {
 
-    } else if ( key.name == 'left' ) {
+            console.log('Left');
+            left_wheel.ccw();
+            right_wheel.ccw();      
 
-        console.log('Left');
-        left_wheel.ccw();
-        right_wheel.ccw();      
+        } else if ( key.name == 'right' ) {
 
-    } else if ( key.name == 'right' ) {
+            console.log('Right');
+            left_wheel.cw();
+            right_wheel.cw();
 
-        console.log('Right');
-        left_wheel.cw();
-        right_wheel.cw();
+        } else if ( key.name == 'space' ) {
 
-    } else if ( key.name == 'space' ) {
+            console.log('Stopping');
+            left_wheel.to(LSTOPVAL);
+            right_wheel.to(RSTOPVAL);
 
-        console.log('Stopping');
-        left_wheel.to(LSTOPVAL);
-        right_wheel.to(RSTOPVAL);
-
-    } else if (key.name == "o") {
-        console.log("on");
-        led.on();
-    } else if (key.name == "f") {
-        console.log("off");
-        led.off();
-    } else if (key.name == "s") {
-        console.log("blink");
-        led.strobe(2000);
-    }
-  });
+        }
+    });
 });
 
 board.on("error", function(err) {
