35d3227a2bc3e504c9601bc968bb4d61186fe54e
ajfisher <ajfisher.td@gmail.com>
2016-07-10 21:51:16 +1000

    big refactor of the firmware build and folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,21 @@
 module.exports = function(grunt) {
- 
+
     // configure the tasks
     grunt.initConfig({
+        clean: {
+            firmware_build: {
+                src: [
+                        'firmware/build/standard/simplebot_firmata',
+                        'firmware/build/sbs/sbs_firmata',
+                        'firmware/build/network/simplebot_firmata',
+                     ]
+            },
+            compiled_bins: {
+                src: [
+                        'firmware/bin/*'
+                    ]
+            },
+        },
         copy: {
             options: {
                 timestamp: true,
@@ -11,7 +25,7 @@ module.exports = function(grunt) {
                 cwd: 'firmware/src/',
                 flatten: true,
                 src: [ 'libs/firmata/**', 'controller_src/simplebot_firmata/*' ],
-                dest: 'firmware/build/simplebot_firmata/',
+                dest: 'firmware/build/standard/simplebot_firmata/',
                 expand: true,
                 filter: 'isFile',
             },
@@ -19,38 +33,44 @@ module.exports = function(grunt) {
                 cwd: './',
                 flatten: true,
                 src: [  'firmware/src/libs/firmata/**',
-                        'firmware/src/controller_src/sbs_firmata/*', 
+                        'firmware/src/controller_src/sbs_firmata/*',
                         'node_modules/node-pixel/firmware/src/libs/neopixel/*',
                         'node_modules/node-pixel/firmware/src/libs/ws2812/*'
                 ],
-                dest: 'firmware/build/sbs_firmata/',
+                dest: 'firmware/build/sbs/sbs_firmata/',
                 expand: true,
                 filter: 'isFile',
             },
             network_simplebot_firmata: {
                 cwd: 'firmware/src/',
                 flatten: true,
-                src: [ 'libs/firmata/**', 'controller_src/network_simplebot_firmata/*' ],
-                dest: 'firmware/build/network_simplebot_firmata/',
+                src: [ 'libs/firmata/**', 'controller_src/simplebot_firmata/*' ],
+                dest: 'firmware/build/network/simplebot_firmata/',
                 expand: true,
                 filter: 'isFile',
             },
         },
-        clean: {
-            build: {
-                src: [  
-                        'firmware/build/simplebot_firmata',
-                        'firmware/build/sbs_firmata',
-                        'firmware/build/network_simplebot_firmata',
-                     ]
+        'string-replace': {
+            precompile: {
+                files: [{
+                    src: 'firmware/build/network/simplebot_firmata/simplebot_firmata.ino',
+                    dest: 'firmware/build/network/simplebot_firmata/simplebot_firmata.ino',
+                }],
+                options: {
+                    replacements: [{
+                        pattern: /57600/,
+                        replacement: '115200',
+                    }],
+                },
             },
         },
     });
- 
+
     // load the tasks
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-string-replace');
 
-    grunt.registerTask('build', ['clean', 'copy']);
+    grunt.registerTask('build', ['clean:firmware_build', 'clean:compiled_bins', 'copy', 'string-replace']);
 
 };
