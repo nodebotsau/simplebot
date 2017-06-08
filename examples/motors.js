c6d69076110bbf9b317b80e23ca83ae48c8351d3
ajfisher <ajfisher.td@gmail.com>
2017-06-08 10:17:03 +1000

    Simplified motor example

--- examples/motors.js
+++ examples/motors.js
@@ -1,22 +1,16 @@
-var five = require("johnny-five");
-var keypress = require("keypress");
+const five = require("johnny-five");
+const keypress = require("keypress");
 
-var board, motor_l, motor_r;
-var servo_l, servo_r;
-var led;
+let board, motor_l, motor_r;
 
-var speed = 0;
-var min_speed = 230;
-var max_speed = 255;
-var cur_speed_setting = 0.9
-
+const speed = 255;
 
 // set up the input
 keypress(process.stdin);
 
-process.stdin.resume(); 
-process.stdin.setEncoding('utf8'); 
-process.stdin.setRawMode(true); 
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.setRawMode(true);
 
 console.info("Setting up robot. Attempting J5 connect to Arduino")
 
@@ -30,15 +24,6 @@ board.on("ready", function(err) {
     }
 
     console.info("Board connected. Robot set up");
-	var ir = new five.IR.Reflect.Array({
-	  emitter: 13,
-	  pins: ["A0", "A1"], // any number of pins
-	  freq: 1000
-	});
-
-	ir.on('data', function() {
-    	console.log( "Raw Values: ", this.raw );
-  	});
 
     motor_r = new five.Motor({
         pins: {
@@ -56,13 +41,8 @@ board.on("ready", function(err) {
         invertPWM: true,
     });
 
-    servo_l = new five.Servo(3);
-    servo_r = new five.Servo(4);
-
-    led = new five.Led(11);
-
 	console.info("Robot running issue commands to it.");
-	console.info("LRUD arrows. Space stop. H help");
+	console.info("LRUD arrows. Space stop. C to quit");
 
 });
 
@@ -75,9 +55,6 @@ process.stdin.on('keypress', function(chunk, key) {
         motor_r.stop();
 		process.exit();
 	}
-
-	speed = (cur_speed_setting * (max_speed - min_speed)) + min_speed;
-    console.log(speed);
 
 	if (key) {
 		switch (key.name) {
@@ -101,53 +78,6 @@ process.stdin.on('keypress', function(chunk, key) {
 				motor_r.stop();
                 motor_l.stop();
 				break;
-			case "1":
-				cur_speed_setting = 0.1;
-				break;
-			case "2":
-				cur_speed_setting = 0.2;
-				break;
-			case "3":
-				cur_speed_setting = 0.3;
-				break;
-			case "4":
-				cur_speed_setting = 0.4;
-				break;
-			case "5":
-				cur_speed_setting = 0.5;
-				break;
-			case "6":
-				cur_speed_setting = 0.6;
-				break;
-			case "7":
-				cur_speed_setting = 0.7;
-				break;
-			case "8":
-				cur_speed_setting = 0.8;
-				break;
-			case "9":
-				cur_speed_setting = 0.9;
-				break;
-            case "0":
-                cur_speed_setting = 1.0;
-                break;
-            case "s":
-                servo_l.to(30);
-                servo_r.to(30);
-                console.log("Sweep the servos");
-                break;
-            case "k":
-                servo_l.stop();
-                servo_r.stop();
-                break;
-            case "l":
-                console.log("LED ON");
-                led.on();
-                break;
-            case "o":
-                console.log("LED OFF");
-                led.off();
-                break;
 		}
     }
 
